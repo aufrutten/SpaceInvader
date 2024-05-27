e69fbf749e47bca5a427d16ebc76e731f910e931
Luca Dragoni <luca.dragoni2006@gmail.com>
2024-05-27 17:44:22 +0200

    renaming sprites

--- src/menu/gamepanels/PlayingPanel.java
+++ src/menu/gamepanels/PlayingPanel.java
@@ -1,23 +1,17 @@
 package menu.gamepanels;
 
 import javax.swing.*;
+
 import java.awt.*;
 
 import static menu.ScrollingImagesPanel.PANEL_HEIGHT;
 import static menu.ScrollingImagesPanel.PANEL_WIDTH;
 
 public class PlayingPanel extends JPanel {
+    private Image playerSpaceShip;
     public PlayingPanel() {
         setLayout(null);
         setOpaque(false);
         setBounds(0, 0, PANEL_WIDTH, PANEL_HEIGHT);
-
-        JLabel messageLabel = new JLabel("IMPLEMENTATION NEEDED");
-        messageLabel.setHorizontalAlignment(JLabel.CENTER);
-        messageLabel.setVerticalAlignment(JLabel.CENTER);
-        messageLabel.setFont(messageLabel.getFont().deriveFont(20f));
-        messageLabel.setForeground(Color.WHITE);
-        messageLabel.setBounds(0, 0, PANEL_WIDTH, PANEL_HEIGHT);
-        add(messageLabel);
     }
 }
